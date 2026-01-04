22d31878732372934dbecaaea96db396e2b4e989
Bianca <dancibianca1@gmail.com>
2026-01-04 21:31:01 +0200

    Created [project

---
+++ gateway/src/main/java/com/barloyalty/gateway/BarLoyaltyApplication.java
@@ -0,0 +1,13 @@
+package com.barloyalty.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarLoyaltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarLoyaltyApplication.class, args);
+	}
+
+}
